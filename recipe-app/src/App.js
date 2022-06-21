b5fdb586a9ae5f205ea7827d7085c207023b8f4f
GeorgiPavlov89 <georgi.pavlov89@gmail.com>
2022-06-21 10:34:52 +0300

    Search functionality

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,11 +1,13 @@
 import Pages from './pages/Pages'
 import Category from './compopnents/Category'
 import { BrowserRouter } from 'react-router-dom'
+import Search from './compopnents/Search'
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
+      <Search />
       <Category />
       <Pages />
       </BrowserRouter>
